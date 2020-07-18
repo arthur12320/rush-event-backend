fbfda0bb15e563ce07da5f8f6ea942da6e9ecb69
arthurraragao@hotmail.com <=>
2020-07-18 19:10:58 -0300

    modificado email

--- routes.js
+++ routes.js
@@ -8,7 +8,24 @@ sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 router.post('/', function (req, res, next) {
     try {
-        const { email, subject, text, html } = req.body;
+        const { email, name, obs, area } = req.body;
+        const subject = 'especialização - ' + area;
+        const html = `
+            <p>
+            nome: ${name},
+            email: ${email},
+            observaçõe: ${obs},
+            area: ${area}
+            </p>
+        `;
+
+        const text = `
+            nome: ${name},
+            email: ${email},
+            observaçõe: ${obs},
+            area: ${area}
+        `;
+
         sgMail.send({
             to: email,
             from: 'arthurraragao@hotmail.com',
@@ -19,7 +36,7 @@ router.post('/', function (req, res, next) {
         res.send(200);
     } catch (err) {
         res.send(500);
-    }   
+    }
 });
 
 module.exports = router;
